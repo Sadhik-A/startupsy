ef5488c5f54a804b5f51d7c8da4e6967e377978f
Sadhik-A <sadhi0204@gmail.com>
2025-01-21 20:48:06 +0530

    feat: add details page

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/query";
+import { notFound } from "next/navigation";
+
+export const experimental_ppr = true;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id: id });
+  if (!post) return notFound();
+  return <p>{post.title}</p>;
+};
+export default Page;
